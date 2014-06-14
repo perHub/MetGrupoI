79f23f1e4f758fceb908b2fac28102464a877daa
yanina <yanina86@gmail.com>
2014-06-14 12:46:54 -0300

    test traer todos nhibernate

---
+++ Testing/NHibernateTestTraerTodos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using NHibernate;
+using NUnit.Framework;
+using Entidades;
+
+namespace Testing
+{
+    [TestFixture]
+    class NHibernateTestTraerTodos
+    {
+        Configuration cfg;
+        ISession sesion;
+        ISessionFactory fab_sesion;
+
+        [TestFixtureSetUp]
+        public void init()
+        {
+            cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof(Empresa).Assembly);
+            //new SchemaExport(cfg).Execute(true, true, false); // Crearía las tablas.
+            fab_sesion = cfg.BuildSessionFactory();
+            sesion = fab_sesion.OpenSession();
+        }
+        [SetUp]
+        public void setup()
+        {
+            sesion = fab_sesion.OpenSession();
+        }
+       [Test]
+        public void NhibernateTestGetAll()
+        {
+            int cant = sesion.QueryOver<Empresa>().RowCount();
+            var empresas = sesion.CreateQuery("from Empresa").List<Empresa>();
+            Assert.AreEqual(cant, empresas.Count);
+        }
+
+    }
+}
