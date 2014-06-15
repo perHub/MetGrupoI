f3e91d3a1e4b80af5eb8364eff909d3db56e9d09
yanina <yanina86@gmail.com>
2014-06-15 15:10:27 -0300

    test perdido

---
+++ Testing/NHibernateModificarEmpresa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using NHibernate;
+using NUnit.Framework;
+using Entidades;
+
+
+namespace Testing
+{
+    class NHibernateModificarEmpresa
+    {
+        Configuration cfg;
+        ISession sesion;
+        ISessionFactory fab_sesion;
+        Empresa e;
+
+        [TestFixtureSetUp]
+        public void init()
+        {
+            cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof(Empresa).Assembly);
+            //new SchemaExport(cfg).Execute(true, true, false); // Crearía las tablas.
+            fab_sesion = cfg.BuildSessionFactory();
+            sesion = fab_sesion.OpenSession();
+            e = new Empresa(1, "aucas libreria", "italia 3973");
+        }
+        [SetUp]
+        public void setup()
+        {
+            sesion = fab_sesion.OpenSession();
+        }
+        [Test]
+        public void NHibernateModificarEmpresas() {
+            sesion.Update(e);
+            var eDEbbdd= sesion.Get<Empresa>(e.Id);
+            sesion.Flush();
+            Assert.AreEqual(e, eDEbbdd);
+        }
+    }
+}
